d59801503f94d1a3fdcc52fc27ce54224dd40df1
Kyrylo Terentiev <kirillterentyev1999@gmail.com>
2022-11-01 13:06:21 +0100

    Implement WeatherApiService and controller logic, unit tests and weatherWidget to show current weather. permission set with control

--- force-app/main/default/lwc/weatherWidget/weatherWidget.js
+++ force-app/main/default/lwc/weatherWidget/weatherWidget.js
@@ -1,27 +1,128 @@
 import { LightningElement, api, wire, track } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import weatherApiLogo from '@salesforce/resourceUrl/WeatherApiLogo';
-
+import getForecastByZip from '@salesforce/apex/WeatherApiController.getForecastByZip';
 
 export default class WeatherWidget extends LightningElement {
 
     @track zip;
     @track weatherData;
+    @track isMetric = false;
     @track isLoading = false;
+    @track activeSections = ['Current'];
 
     get logo() {
         return weatherApiLogo;
     }
 
     handleZipChange(event) {
-        this.zip = event.detail.value;
+        this.zip = event.target.value;
         this.template.querySelector('[data-id="zip"]').reportValidity();
     }
 
+    handleMetricChange(event) {
+        this.isMetric = event.target.checked;
+    }
+
     handleGetForecastClick(event) {
-        // this.isLoading = true;
+        this.weatherData = undefined;
+        this.isLoading = true;
+        getForecastByZip({zip: this.zip})
+            .then(result => {
+                this.weatherData = result;
+                this.isLoading = false;
+            })
+            .catch(error => {
+                this.showToast('Error', error.body.message, 'error');
+                this.isLoading = false;
+            });
+    }
+
+    handleSectionToggle(event) {
+
     }
 
     get isGetForecastDisabled() {
         return !(!this.isLoading && this.zip && this.template.querySelector('[data-id="zip"]') && this.template.querySelector('[data-id="zip"]').validity.valid);
+    }
+
+    get location() {
+        if (this.weatherData && this.weatherData.location) {
+            return this.weatherData.location.name + ', ' + this.weatherData.location.region;
+        }
+    }
+
+    get temperature() {
+        let result;
+        if (this.weatherData && this.weatherData.current) {
+            result = this.isMetric ? this.weatherData.current.temp_c : this.weatherData.current.temp_f;
+            result += this.isMetric ? '°C' : '°F';
+        }
+        return result;
+    }
+
+    get feelsLike() {
+        let result;
+        if (this.weatherData && this.weatherData.current) {
+            result = this.isMetric ? this.weatherData.current.feelslike_c : this.weatherData.current.feelslike_f;
+            result += this.isMetric ? '°C' : '°F';
+        }
+        return result;
+    }
+
+    get precipation() {
+        let result;
+        if (this.weatherData && this.weatherData.current) {
+            result = this.isMetric ? this.weatherData.current.precip_mm : this.weatherData.current.precip_in;
+            result += this.isMetric ? ' mm' : ' in';
+        }
+        return result;
+    }
+
+    get humidity() {
+        if (this.weatherData && this.weatherData.current) {
+            return this.weatherData.current.humidity + '%';
+        }
+    }
+
+    get wind() {
+        let result;
+        if (this.weatherData && this.weatherData.current) {
+            result = this.isMetric ? this.weatherData.current.wind_kph : this.weatherData.current.wind_mph;
+            result += this.isMetric ? ' km/h' : ' m/h';
+        }
+        return result;
+    }
+
+    get weatherText() {
+        if (this.weatherData && this.weatherData.current && this.weatherData.current.condition) {
+            return this.weatherData.current.condition.text;
+        }
+    }
+
+    get weatherIcon() {
+        if (this.weatherData && this.weatherData.current && this.weatherData.current.condition) {
+            return 'https:' + this.weatherData.current.condition.icon;
+        }
+    }
+
+    get localTime() {
+        if (this.weatherData && this.weatherData.location) {
+            return this.weatherData.location.localtime;
+        }
+    }
+
+    get lastUpdated() {
+        if (this.weatherData && this.weatherData.current) {
+            return this.weatherData.current.last_updated;
+        }
+    }
+
+    showToast(title, message, variant) {
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title, message, variant
+            })
+        );
     }
 }
